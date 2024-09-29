1d60488d6ee773ef70ef280d56cb32d6216bf8fd
Zuly0926 <zulyalvear0926@gmail.com>
2024-09-28 19:49:12 -0500

    Ejercicio Nro.4

--- Persona.ts
+++ Persona.ts
@@ -1,14 +1,18 @@
 
 export class Persona {
     Nombre: string
-    Edad: number
+   private Edad: number
 
     constructor(Nombre: string, Edad: number){
         this.Nombre = Nombre
         this.Edad = Edad
     }
 
+    getEdad(): number {
+        return this.Edad
+    }
+
     saludar(): void {
-        console.log(`Persona: Tu nombre es ${this.Nombre} y tienes ${this.Edad} años.`)
+        console.log(`Persona: Tu nombre es ${this.Nombre} y tienes ${this.getEdad} años.`)
     }
 }
