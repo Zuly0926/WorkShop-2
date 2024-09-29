0ce1a78e194d010792554364b1d4fddfa85669bc
Zuly0926 <zulyalvear0926@gmail.com>
2024-09-28 19:40:54 -0500

    Ejercicio Nro.1

---
+++ Persona.ts
@@ -0,0 +1,14 @@
+
+class Persona {
+    Nombre: string
+    Edad: number
+
+    constructor(Nombre: string, Edad: number){
+        this.Nombre = Nombre
+        this.Edad = Edad
+    }
+
+    saludar(): void {
+        console.log(`Persona: Tu nombre es ${this.Nombre} y tienes ${this.Edad} años.`)
+    }
+}
