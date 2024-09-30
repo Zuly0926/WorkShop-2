f6418a6a2fa38f16c6685850fc532c6aa493da7a
Zuly0926 <zulyalvear0926@gmail.com>
2024-09-30 10:55:07 -0500

    Ejercicio Nro.12

---
+++ JsonEmpleado.ts
@@ -0,0 +1,13 @@
+
+const empleadosJson = [
+    { nombre: "Juan", edad: 25, direccion: { calle: "Av. Siempre Viva", ciudad: "Springfield", pais: "USA" }, salario: 3000 },
+    { nombre: "Maria", edad: 30, direccion: { calle: "Calle Luna", ciudad: "Bogotá", pais: "Colombia" }, salario: 4000 },
+    { nombre: "Pedro", edad: 35, direccion: { calle: "Calle Sol", ciudad: "Buenos Aires", pais: "Argentina" }, salario: 4500 },
+    { nombre: "Ana", edad: 28, direccion: { calle: "Calle Estrella", ciudad: "Lima", pais: "Perú" }, salario: 3500 },
+    { nombre: "Luis", edad: 32, direccion: { calle: "Calle Cielo", ciudad: "Ciudad de México", pais: "México" }, salario: 5000 },
+    { nombre: "Sofia", edad: 27, direccion: { calle: "Calle Mar", ciudad: "Madrid", pais: "España" }, salario: 3300 },
+    { nombre: "Diego", edad: 31, direccion: { calle: "Calle Tierra", ciudad: "Montevideo", pais: "Uruguay" }, salario: 3700 },
+    { nombre: "Laura", edad: 29, direccion: { calle: "Calle Aire", ciudad: "Santiago", pais: "Chile" }, salario: 3800 },
+    { nombre: "David", edad: 33, direccion: { calle: "Calle Fuego", ciudad: "Asunción", pais: "Paraguay" }, salario: 4200 },
+    { nombre: "Elena", edad: 26, direccion: { calle: "Calle Agua", ciudad: "Caracas", pais: "Venezuela" }, salario: 3600 }
+]
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Coche, Moto, Vehiculo } from "./claseAbstracta";
+import { Coche, Moto } from "./claseAbstracta";
 import { Empleado } from "./Empleado";
 import { Direccion } from "./interface";
 import { Persona } from "./Persona";
@@ -6,7 +6,7 @@ import { crearJsonYTransformar } from "./util";
 
 const personas: Persona[] = []
 for (let i = 1; i <= 10; i++) {
-    const direccion: Direccion = { Calle: `Cslle ${i}`, Ciudad: `Ciudad ${i}`, Pais: `Pais`}
+    const direccion: Direccion = { Calle: `Cslle ${i}`, Ciudad: `Ciudad ${i}`, Pais: `Pais ${i}`}
     const persona = new Persona(`Persona ${i}`, 20 + i, direccion)
 
     if (i % 2 === 0) {
@@ -24,20 +24,11 @@ personas[1].vehiculos.forEach(Vehiculo  => Vehiculo.arrancar())
 
 const empleados: Empleado[] = []
 for (let i = 1; i <= 10; i++) {
-    const direccion: Direccion = { Calle: `Calle ${i}`, Ciudad: `Ciudad ${i}`, Pais: `Pais`}
+    const direccion: Direccion = { Calle: `Calle ${i}`, Ciudad: `Ciudad ${i}`, Pais: `Pais ${i}`}
     const empleado = new Empleado(`Empleado ${i}`, 25 + 1, 30000 + i * 10000, direccion)
-
-    if (i % 2 === 0) {
-        empleado.agregarVehiculo(new Coche());
-    } else {
-        empleado.agregarVehiculo(new Moto());
-    }
 
     empleados.push(empleado)
 }
 
 empleados.forEach(empleado => empleado.saludar())
-empleados[0].vehiculos.forEach(Vehiculo => Vehiculo.arrancar())
-empleados[1].vehiculos.forEach(Vehiculo => Vehiculo.arrancar())
-
 crearJsonYTransformar
